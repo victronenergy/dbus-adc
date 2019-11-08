84d8b153833e09f34707aaa8b09b48e3847a1647
Mans Rullgard <mans@mansr.com>
2019-11-08 12:37:06 +0000

    disable velib serial support

--- software/inc/velib/velib_config_app.h
+++ software/inc/velib/velib_config_app.h
@@ -52,13 +52,6 @@
 #define CFG_WITH_LOG						1
 #define CFG_LOG_STAMP_DEC					1
 
-/* compile in serial support */
-#define CFG_WITH_SERIAL						1
-/* and open the port at startup */
-#define CFG_INIT_SERIAL						1
-
-#define CFG_SERIAL_RX_BUFFER_LEN			2048
-
 /* version and banner commandline option */
 #define CFG_WITH_VERSION					1
 
